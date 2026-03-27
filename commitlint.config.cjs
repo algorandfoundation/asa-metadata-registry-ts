cf6b0ff5e7d1f7911645abd184b9a877588ed290
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-03-27 11:28:14 +1000

    chore: PR feedback

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,9 +2,9 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // Allow sentence case commit messages
-    'subject-case': [1, 'always', ['pascal-case', 'upper-case']],
+    'subject-case': [1, 'always', ['sentence-case', 'pascal-case', 'upper-case']],
     'type-empty': [1, 'never'],
-    'subject-empty': [1, 'always'],
+    'subject-empty': [0],
     'body-leading-blank': [0, 'always'],
     'body-max-line-length': [1, 'always', 200],
     'header-max-length': [1, 'always', 150],
